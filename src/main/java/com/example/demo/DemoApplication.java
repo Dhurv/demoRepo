b447f6cf5dd0fb151f38257e5a01a21b05525714
dhrubajyoti_bharali <official.dhruvb@gmail.com>
2022-02-13 12:51:39 +0530

    Add another comment in the main class

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 // this is the main java class
+//	this is another comment
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
